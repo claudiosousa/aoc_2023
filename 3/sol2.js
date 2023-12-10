772e57ca8158bc81bd94990d8b9b35a4c962d4b4
Claudio <claudio.sousa@gmail.com>
2023-12-10 16:23:30 +0100

    sol 3.2

--- 3/sol2.js
+++ 3/sol2.js
@@ -1,24 +1,19 @@
+import { match } from 'node:assert';
 import { promises as fs } from 'node:fs';
 
 const lines = (await fs.readFile('./input.txt', 'utf-8')).toString().split('\n');
-let res = 0;
-for (let line of lines) {
-    let r = line.match('Game (\\d+): (.*)');
-    //let id = r[1];
-    let matches = r[2];
-    let match;
-    let red = 0, green = 0, blue = 0;
-    while (match = matches.match('(\\d+) (red|blue|green)')) {
-        let nb = parseInt(match[1]);
-        let color = match[2];
-        matches = matches.substring(match.index + match[0].length)
-        if (color == 'red' && nb > red)
-            red = nb;
-        if (color == 'green' && nb > green)
-            green = nb;
-        if (color == 'blue' && nb > blue)
-            blue = nb;
-    }
-    res += red * green * blue;
-}
-console.log(res)
+const caracters = lines.map(l => l.split(''));
+const maybeGears = caracters.flatMap((row, ir) =>
+    row.map((c, ic) => c == '*' ? [ir, ic] : null)
+        .filter(v => v));
+
+const numbers = lines.flatMap((row, ir) =>
+    [...row.matchAll(/\d+/g)].map(match => ({ col: match.index, row: ir, v: parseInt(match['0']), length: match['0'].length })));
+
+const gears = maybeGears.map(([cr, cc]) =>
+    numbers.filter(({ col, row, v, length }) => Math.abs(cr - row) <= 1 && cc >= col - 1 && cc <= col + length))
+    .filter(mg => mg.length == 2)
+    .map(([n1, n2]) => n1.v * n2.v)
+    .reduce((v, acc) => v + acc, 0);
+    
+console.log(gears);
