b48b04b9f1d87b60968eeddb0f42859ba24ac3d0
Claudio <claudio.sousa@gmail.com>
2023-12-17 17:47:59 +0100

    sol 5.1

--- 5/sol1.js
+++ 5/sol1.js
@@ -1,6 +1,17 @@
 import { promises as fs } from 'node:fs';
 
-let [seeds, ...lines] = (await fs.readFile('./input.txt', 'utf-8')).toString().split('seeds:')[1].split(/\n[a-z][^:]*:\n/);
+let [seeds, ...maps] = (await fs.readFile('./input.txt', 'utf-8')).toString().split('seeds:')[1].split(/\n[a-z][^:]*:\n/);
 seeds = seeds.replace('\n', '').trim().split(' ').map(n => parseInt(n));
-lines = lines.map(l => l.trim('\n').split('\n').map(l => l.split(' ').map(n => parseInt(n))));
-console.log(seeds, lines);
+maps = maps.map(l => l.trim('\n').split('\n').map(l => l.split(' ').map(n => parseInt(n))));
+//console.log(seeds, maps);
+const convert = (inp, m) => {
+    // console.log(inp, m);
+    let res = m.map(([dest, source, length]) =>
+        inp >= source && inp < source + length ? dest + inp - source : null
+    ).reduce((acc, v) => v != null ? v : acc, inp);
+    //console.log(inp, '=>', res);
+    return res;
+};
+
+const locations = seeds.map(s => maps.reduce((inp, m) => convert(inp, m), s));
+console.log(locations.reduce((acc, v) => v < acc ? v : acc, Number.MAX_VALUE));
