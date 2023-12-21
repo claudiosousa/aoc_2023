ee54e20a87caf3c9d9247b504542ed52daa47168
Claudio <claudio.sousa@gmail.com>
2023-12-21 14:58:47 +0100

    sol 11.1

---
+++ 11/sol1.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'node:fs';
+//import { readFileSync } from 'fs';
+
+let map = readFileSync('./input.txt', 'utf-8').toString().split('\n').map(l => l.split(''));
+const expandUniverse = () => {
+    let j = 0;
+    while (j < map[0].length - 1) {
+        if (map.every(l => l[j] == '.')) {
+            map = map.map(l => l.splice(j, 0, '.') && l)
+            j++;
+        }
+        j++;
+    }
+    let i = 0;
+    while (i < map.length - 1) {
+        if (map[i].every(c => c == '.')) {
+            map.splice(i, 0, map[i].map(v => v))
+            i++;
+        }
+        i++;
+    }
+}
+expandUniverse();
+
+const galaxies = map.reduce((lst, l, i) => [...lst, ...l.map((c, j) => c == '#' ? [i, j] : null).filter(v => v)], []);
+
+let totalDist = 0;
+for (let i = 0; i < galaxies.length; i++) {
+    const gi = galaxies[i];
+    for (let j = i + 1; j < galaxies.length; j++) {
+        const gj = galaxies[j];
+        totalDist += Math.abs(gi[0] - gj[0]) + Math.abs(gi[1] - gj[1])
+    }
+} 
+console.log(totalDist);
