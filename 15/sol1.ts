69d4add59dbcf0b86e7aab778016dc605e15b3ab
Claudio <claudio.sousa@gmail.com>
2023-12-28 15:42:40 +0100

    sol 15.1

---
+++ 15/sol1.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'node:fs';
+
+let instructions = readFileSync('./input.txt', 'utf-8').toString()
+    .split(',');
+
+const hash = (s: string) => {
+    let v = 0;
+    for (let i = 0; i < s.length; i++) {
+        v += s.charCodeAt(i);
+        v *= 17;
+        v %= 256;
+    }
+    return v;
+}
+
+//console.log(instructions.map(step => hash(step)));
+console.log(instructions.map(step => hash(step)).reduce((sum, v) => sum + v, 0));
