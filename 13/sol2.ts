b0b7e1ea5059d1145605653ca266fb829a15b04c
Claudio <claudio.sousa@gmail.com>
2023-12-28 13:30:17 +0100

    sol 13.2

---
+++ 13/sol2.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from 'node:fs';
+
+let maps = readFileSync('./input.txt', 'utf-8').toString()
+    .split('\n\n')
+    .map(l => l.split('\n')
+        .map(l => l.split('')));
+
+const areRowsReflection = (irow, map) => {
+    let smudges = 0;
+    for (let i = 0; i <= irow; i++) {
+        if (i + irow + 1 >= map.length)
+            break;
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[irow - i][j] != map[irow + i + 1][j])
+                smudges++;
+        }
+    }
+    return smudges == 1;
+}
+
+const areColumnsReflection = (icol, map) => {
+    let smudges = 0;
+    for (let i = 0; i <= icol; i++) {
+        if (i + icol + 1 >= map[0].length)
+            break;
+        for (let j = 0; j < map.length; j++) {
+            if (map[j][icol - i] != map[j][icol + i + 1])
+                smudges++;
+        }
+    }
+    return smudges == 1;
+}
+
+const colsRowsBeforeReflection = (map, i) => {
+    for (let i = 0; i < map.length - 1; i++) {
+        if (areRowsReflection(i, map))
+            return 100 * (i + 1);
+    }
+    for (let i = 0; i < map[0].length - 1; i++) {
+        if (areColumnsReflection(i, map))
+            return i + 1;
+    }
+    return 0;
+}
+
+console.log(maps.map((map, i) => colsRowsBeforeReflection(map, i)).reduce((sum, v) => sum + v, 0));
